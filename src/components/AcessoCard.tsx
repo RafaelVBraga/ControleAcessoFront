52418779cfe48041503135f0b6db7cc5b73189d3
RafaelVBraga <rafael.vieira.braga@gmail.com>
2022-05-23 09:47:55 -0300

    AcessoCard Updated with <table>

--- src/components/AcessoCard.tsx
+++ src/components/AcessoCard.tsx
@@ -1,9 +1,26 @@
+import Style from './AcessoCard.module.css'
 function AcessoCard(props:any){
     return(
-        <div>
+        <div className={Style.body}>
             <h1> {props.codigo}</h1>
             <p> {props.descricao} </p>
             <p> {props.data}</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>RG</th>
+                        <th>Matricula</th>
+                        <th>Empresa</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>Rafael</td>
+                    <td>97002520322</td>
+                    <td>269278</td>
+                    <td>Chesf</td>
+                </tr>
+            </table>
             
         </div>)
 }export default AcessoCard;
